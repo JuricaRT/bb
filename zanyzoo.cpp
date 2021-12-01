f344bc0d96fdcf1bae7be7c49acca5fbafa6c23e
JuricaRT <runtas.j@gmail.com>
2021-12-01 13:13:12 +0100

    finish implementation, fix bugs, for 2x2 -> 27 machines, doesn't work yet

--- zanyzoo.cpp
+++ zanyzoo.cpp
@@ -1,8 +1,13 @@
 #include <cstdio>
 #include <vector>
+#include <queue>
+
+#define DEBUG
 
 #define N 2 // states
-#define M 4 // symbols
+#define M 2 // symbols
+
+#define NUMBER_OF_EXECUTION_STEPS 200
 
 #define UNDEFINED_OUTPUT -1
 #define UNDEFINED_STATE -2
@@ -19,18 +24,24 @@
 #define SECOND_STATE 'b'
 #define THIRD_STATE 'c'
 #define BLANK_SYMBOL 0
+#define UNDEFINED_SYMBOL 'U'
 
 #define D(DIRECTION) (DIRECTION == RIGHT ? RIGHT_V : LEFT_V)
-#define S(STATE) (STATE-START_STATE) // converts state to a number
+#define S(STATE) (STATE == HALT_STATE ? HALT_STATE_V : STATE-START_STATE) // converts state to a number
 
 #define rS(x) (x == HALT_STATE_V ? HALT_STATE : x+START_STATE) // convert back
+#define rD(x) (x == RIGHT_V ? RIGHT : LEFT)
+
+class Machine;
+std::priority_queue<Machine> machines_to_consider;
+std::vector<Machine> generated_machines;
 
 class Tape {
   private:
     // nodes instead of vectors because I like pointers
     struct Node {
-      int data;
       int index;
+      int data;
       struct Node *previous;
       struct Node *next;
     };
@@ -42,6 +53,21 @@ class Tape {
   public:
     Tape() : current_node(new Node{0, 0, nullptr, nullptr}), min_index(current_node->index), max_index(current_node->index) {};
     
+    bool is_blank() {
+      bool blank = true;
+      struct Node *node = current_node;
+      // go to the leftmost node
+      while (node->previous != nullptr) node = node->previous;
+      for (int i = min_index; i <= max_index; ++i) {
+        if (node->data != 0) {
+          blank = false;
+          break;
+        }
+        node = node->next;
+      }
+      return blank;
+    }
+
     // this works because Turing machine moves ONLY ONE step to the left/right
     int& operator[](int index) {
       if (index >= min_index && index <= max_index) {
@@ -53,23 +79,22 @@ class Tape {
         }
       } 
       else {
-        int new_index;
-        struct Node *new_previous, *new_next;
+        Node *new_node = new Node{0, 0, nullptr, nullptr};
         if (index > max_index) {
           max_index += 1;
-          new_index = max_index;
-          new_previous = current_node;
-          new_next = nullptr;
+          new_node->index = max_index;
+          new_node->previous = current_node;
+          current_node->next = new_node;
         }
         else if (index < min_index) {
           min_index -= 1;
-          new_index = min_index;
-          new_previous = nullptr;
-          new_next = current_node;
+          new_node->index = min_index;
+          new_node->next = current_node;
+          current_node->previous = new_node;
         }
-        Node *new_node = new Node{0, new_index, new_previous, new_next};
         current_node = new_node; 
       }
+      
       return current_node->data;
     };
 };
@@ -89,19 +114,22 @@ class Transition {
 
 class Machine {
   private:
-    int current_state;
-    int current_position;
     int number_of_states;
     int number_of_symbols;
-    Transition transition_function[N][M];
-    Tape tape;
 
   public:
+    int current_state;
+    int current_position;
+    int activity;
+    Transition transition_function[N][M];
+    Tape tape;
+    
     Machine() {
-      current_state = UNDEFINED_STATE;
+      current_state = S(START_STATE);
       current_position = 0;
-      number_of_states = 0; // 1 but we have to start from 0 because transition_function indexing (S('a') = 0)
-      number_of_symbols = 0; // same thing
+      number_of_states = 0;
+      number_of_symbols = 0;
+      activity = 0;
     }
 
     void add_transition(int state, int input, int output, int direction, int new_state) {
@@ -139,29 +167,165 @@ class Machine {
       for (int symbol = BLANK_SYMBOL; symbol < how_many_to_output; ++symbol) symbols.push_back(symbol);
       return symbols;
     };
-};
 
+    void execute() {
+      Transition &transition = transition_function[current_state][tape[current_position]];
+      
+      tape[current_position] = transition.output;
+      current_position += transition.direction;
+      current_state = transition.new_state;
+      
+      activity += 1; 
+    };
+  
+    bool is_0_dextrous() {
+      int count = 0;
+      for (int i = 0; i < N; ++i) {
+        if (transition_function[i][0].new_state != UNDEFINED_STATE)
+          count += (transition_function[i][0].direction == D(RIGHT));
+      }
+      return count == N;
+    }
+
+    bool n_of_execution_steps_exceeded() {
+      return activity > NUMBER_OF_EXECUTION_STEPS;
+    }
+
+    bool failed_blank_tape_condition() {
+      return (activity > 0 && tape.is_blank());
+    }
+
+    bool undefined_combination_is_found() {
+      return transition_function[current_state][tape[current_position]].new_state == UNDEFINED_STATE;
+    }
+
+    bool is_n_m_full() {
+      return (number_of_states == N && number_of_symbols == M);
+    }
+
+    bool halted() {
+      return current_state == S(HALT_STATE);
+    }
+
+    bool operator <(const Machine &rhs) const {
+      return activity < rhs.activity;
+    }
+
+    int card() {
+      return number_of_states*number_of_symbols;
+    }
+
+    void print_transition_function() const {
+      for (int i = S(START_STATE); i < N; ++i) {
+        for (int j = 0; j < M; ++j) {
+          Transition t = transition_function[i][j];
+          if (t.new_state == UNDEFINED_STATE) printf("(%c,%d): %c%c%c ", rS(i), j, UNDEFINED_SYMBOL, UNDEFINED_SYMBOL, UNDEFINED_SYMBOL);
+          else {
+            printf("(%c,%d): %d%c%c ", rS(i), j, t.output, rD(t.direction), rS(t.new_state));
+          }
+        }
+        printf("\n");
+      }
+      printf("\n");
+    }
+
+};
 
 void generate() {
   Machine machine;
+  // for state and symbol choice
+  std::vector<int> states, symbols;
   
   // step 1
   machine.add_transition(S(START_STATE), BLANK_SYMBOL, 1, D(RIGHT), S(SECOND_STATE));
   
   // step 2
-  std::vector<int> symbols = machine.symbol_choice();
+  symbols = machine.symbol_choice();
+  
   for (int symbol : symbols) {
      for (char state : {START_STATE, SECOND_STATE}) {
-       machine.add_transition(S(SECOND_STATE), BLANK_SYMBOL, symbol, D(LEFT), state);
+       machine.add_transition(S(SECOND_STATE), BLANK_SYMBOL, symbol, D(LEFT), S(state));
+       machines_to_consider.push(machine);
      }
   }
   
   if (N >= 3) {
     for (int symbol : symbols) {
       for (char direction : {LEFT, RIGHT}) {
-        machine.add_transition(S(SECOND_STATE), BLANK_SYMBOL, symbol, direction, S(THIRD_STATE));
+        machine.add_transition(S(SECOND_STATE), BLANK_SYMBOL, symbol, D(direction), S(THIRD_STATE));
+        machines_to_consider.push(machine);
       }
     } 
+  } 
+   
+  while (!machines_to_consider.empty()) {
+    machine = machines_to_consider.top();
+    machines_to_consider.pop();
+    
+    // step 3
+    // break == halting machine/stopping execution
+    while (true) {
+      Transition &t = machine.transition_function[machine.current_state][machine.tape[machine.current_position]];
+      
+      printf("Total: %lu - Candidates: %lu - Left: %lu --- Steps:%d State:%c Position:%d Input:%d Output:%d Direction:%c NewState:%c\n",
+             machines_to_consider.size()+generated_machines.size(), generated_machines.size(),
+             machines_to_consider.size(), machine.activity, rS(machine.current_state), machine.current_position,
+             machine.tape[machine.current_position], t.output, rD(t.direction), rS(t.new_state));
+      
+      // Clearly we need to store the machines whose computation length which exceeds the given bound, but strictly speaking,
+      // we could insist that machines that are known to be irrelevant are not stored at all.
+      // Whilst this would reduce the number of machines considered, we have chosen to retain such machines in order to
+      // simplify the definition of the generation process.
+      if (machine.n_of_execution_steps_exceeded() || machine.failed_blank_tape_condition()) {
+        generated_machines.push_back(machine);
+        break;
+      }
+      
+      if (machine.undefined_combination_is_found()) {
+        // step 4
+        if (machine.is_n_m_full()) {
+          machine.add_transition(machine.current_state, machine.tape[machine.current_position], 1, D(RIGHT), S(HALT_STATE));
+          if (!machine.is_0_dextrous()) {
+            generated_machines.push_back(machine);
+          }
+        }
+        
+        states = machine.state_choice();
+        symbols = machine.symbol_choice();
+        for (int state : states) {
+          for (int symbol : symbols) {
+            for (char dir : {LEFT, RIGHT}) {
+              // we do not want to alter original machine
+              Machine machine_copy = machine;
+              machine_copy.add_transition(machine.current_state, machine.tape[machine.current_position], symbol, D(dir), state);
+              if (!machine_copy.is_0_dextrous())
+                generated_machines.push_back(machine_copy);
+            }
+          }
+        }
+        break;
+      }
+      
+      // step 5
+      if (machine.card() == N*M-1) {
+        // find appropriate S and I
+        for (int i = 0; i < N; ++i) {
+          for (int j = 0; j < M; ++j) {
+            if (machine.transition_function[i][j].new_state == UNDEFINED_STATE) {
+              machine.add_transition(i, j, 1, D(RIGHT), S(HALT_STATE));
+              generated_machines.push_back(machine);
+            }
+          }
+        }
+        break;
+      }
+      
+      machine.execute();
+      if (machine.halted()) {
+        generated_machines.push_back(machine);
+        break;
+      }
+    }
   }
 }
 
